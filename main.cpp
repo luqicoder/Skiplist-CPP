e955a9916d16f728ad254c5864ff8aae3059df98
luqicoder <49476849+luqicoder@users.noreply.github.com>
2021-08-18 10:59:46 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,3 @@
-/* ************************************************************************
-> File Name:     main.cpp
-> Author:        程序员Carl
-> 微信公众号:    代码随想录
-> Created Time:  Sun Dec  2 20:21:41 2018
-> Description:   
- ************************************************************************/
 #include <iostream>
 #include "skiplist.h"
 #define FILE_PATH "./store/dumpFile"
@@ -15,14 +8,14 @@ int main() {
     // 而且如果修改key的类型，同时需要修改skipList.load_file函数
 	SkipList<int, std::string> skipList(3);
 	skipList.display_list();
-	skipList.insert_element(1, "学"); 
+	skipList.insert_element(1, "数据1"); 
 	skipList.display_list();
-	skipList.insert_element(3, "算法"); 
-	skipList.insert_element(7, "认准"); 
-	skipList.insert_element(8, "微信公众号：代码随想录"); 
-	skipList.insert_element(9, "学习"); 
-	skipList.insert_element(19, "算法不迷路"); 
-	skipList.insert_element(19, "赶快关注吧你会发现详见很晚！"); 
+	skipList.insert_element(3, "数据2"); 
+	skipList.insert_element(7, "数据3"); 
+	skipList.insert_element(8, "数据4"); 
+	skipList.insert_element(9, "数据6"); 
+	skipList.insert_element(19, "数据7"); 
+	skipList.insert_element(19, "数据8"); 
 
     std::cout << "skipList size:" << skipList.size() << std::endl;
 
